d3300980ccc8ee6dddb5477d23e9eb7928fb78c6
Berkaygulmez <berkaybjk04@icloud.com>
2023-09-06 12:30:53 +0300

    AOP ile clean bsuiness kodları yazıldı

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,5 +13,8 @@ namespace Business.Constants
         public static string ProductNameInValid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductsListed="Ürünler listelendi";
+        public static string ProductCountOfCategoryError="Bir Kategoride en fazla 10 ğrğn olabilir";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        public static string CheckIfCategoryLimitExceded="Kategori Limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
