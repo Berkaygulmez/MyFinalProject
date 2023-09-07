8015cd3076ac501992b6426d207449e4116f93c2
Berkaygulmez <berkaybjk04@icloud.com>
2023-09-07 19:09:32 +0300

    Encryption, Hashing ve JWT'ye başlandı

---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim:IEntity
+    {
+        public int Id { get; set; }
+        public int USerId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
