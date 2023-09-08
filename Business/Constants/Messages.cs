0a9883d58803a99ae589ccb4bd310861c9e8871c
Berkaygulmez <berkaybjk04@icloud.com>
2023-09-08 16:43:47 +0300

    JWT devamı

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,7 +1,9 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,7 @@ namespace Business.Constants
 {
     public static class Messages
     {
+        
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInValid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
@@ -16,5 +19,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError="Bir Kategoride en fazla 10 ğrğn olabilir";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
         public static string CheckIfCategoryLimitExceded="Kategori Limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied="Yetkiniz yok";
+        public static string UserRegistered="Kullanıcı Kayıtlı";
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+        public static string PasswordError="Şifre Hatalı";
+        public static string SuccessfulLogin="Giriş işlemi başarılı";
+        public static string UserAlreadyExists="Kullanıcı mevcut";
+        public static string AccessTokenCreated="Token oluşturuldu";
     }
 }
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -5,6 +5,7 @@ using Business.CCS;
 using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -12,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Business.Concrete.UserManager;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -24,6 +26,13 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
 
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
